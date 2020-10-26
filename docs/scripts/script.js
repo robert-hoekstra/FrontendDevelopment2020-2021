c303e55d13eaab04220645ab79b414203fa3dc2d
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-10-26 15:14:57 +0100

    Fixed all feedback from Janno
    Feedback Janno 26-10-2020 verwerkt
    
    W3c errors // verwijderd. Heading levels aan sections toevoegen lijkt mij nu geen goed idee. Deze heb ik qua inhoud niet nodig dus dan zou er een display;none worden gebruikt en daar zijn heel veel partijen weer niet blij mee.
    
    A in een ul // Allemaal weg
    
    Defer async // verwijderd, alleen nog in head aanwezig
    
    Svg inline styles // SVG wordt bij Ajax dus extern ingeladen via een IMG tag. Dat zorgt er voor dat SVG wel responsive is. Dit is tegelijkertijd ook het hekel met de horizontal overflow, die nu als het goed is weg is.
    
    Comments in de code // HTML, CSS en Javascript van Comments voorzien
    
    Javascript is refactored. Variabelen bij variabelen, functies bij functies, event listeners bij event listeners.
    
    Herschrijf window.onscroll // herschreven naar een event listener ('scroll')
    
    Overflow fixen // zat nog een min-width op de body van 420px en een foutje qua svg’s die inline waren geplaatst. zie onderdeel svg
    
    Lang atr toegevoegd
    Folder structuur nagekeken, bleek goed te zijn.
    Alt tags ingevuld van images

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,34 +1,35 @@
-// JavaScript Document
-let toggleButton = document.querySelector('#toggleMenu');
-console.log(toggleButton)
+//// JavaScript Document for micro interactions home and webpage
 
-toggleButton.addEventListener("click", toggleMenu)
+// Variables
+let toggleButton = document.querySelector("#toggleMenu");
+let scrollButton = document.getElementById("scrollButton");
 
-function toggleMenu(){
-    let mobileMenu = document.querySelector("nav");
-    mobileMenu.classList.toggle("show")
+// Event listeners
+scrollButton.addEventListener("click", scrollToTop);
+window.addEventListener("scroll", scrollFunction);
+toggleButton.addEventListener("click", toggleMenu);
+
+// functions
+
+function toggleMenu() {
+  let mobileMenu = document.querySelector("nav");
+  mobileMenu.classList.toggle("show");
 }
 
-let scrollButton = document.getElementById('scrollButton')
-
-scrollButton.addEventListener("click", scrollToTop)
-
-function scrollToTop(){
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 // Onderstaande code kijkt naar de hoogte van de body view. als deze een bepaalt punt voorbij is dan wordt de display stijl aangepast zodat de knop zicht baar is.
 // https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
-
-window.addEventListener("scroll", scrollFunction)
 function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
     scrollButton.style.display = "block";
   } else {
     scrollButton.style.display = "none";
   }
 }
-
-
-
